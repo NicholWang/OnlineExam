374e7eec88fc32e6833e4443a42aa22a22c9a045
NicholWang <1733775867@qq.com>
2020-10-21 03:35:55 +0800

    '首页'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./common.scss";
+
+import "./components/DisplayIndex";
+import DisplayIndex from "./components/DisplayIndex";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <DisplayIndex />
+      </div>
     </div>
   );
 }
---
+++ src/components/DisplayIndex.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import "./style.scss";
+import Button from "@material-ui/core/Button";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+function DisplayIndex() {
+  const [words, setWords] = useState(["Online", "Exam", "System"]);
+  const [wordsIndex, setWordsIndex] = useState(0);
+  const [text, setText] = useState("");
+  const [isdel, setIsDel] = useState(false);
+  const [typeSpeed, setTypeSpeed] = useState(300);
+  let timer = null;
+
+  function typing() {
+    const current = wordsIndex % words.length;
+    const fullTxt = words[current];
+    if (isdel) {
+      setText(fullTxt.substring(0, text.length - 1));
+    } else {
+      setText(fullTxt.substring(0, text.length + 1));
+      console.log(text);
+    }
+    if (!isdel) {
+      setTypeSpeed(typeSpeed / 2);
+    }
+
+    if (!isdel && text === fullTxt) {
+      setTypeSpeed(300);
+      setIsDel(true);
+    } else if (isdel && text === "") {
+      setWordsIndex(wordsIndex + 1);
+      setIsDel(false);
+      setTypeSpeed(500);
+    }
+  }
+
+  useEffect(() => {
+    timer = setInterval(() => typing(), typeSpeed);
+    return () => {
+      clearInterval(timer);
+    };
+  });
+  return (
+    <div className="display-wrapper">
+      <div className="auto-type">
+        welcome to <span className="auto-text">{text}</span>
+      </div>
+      <p>欢迎来到在线考试系统</p>
+      <Button variant="contained" className="btn">
+        立即使用
+        <ChevronRightIcon />
+      </Button>
+    </div>
+  );
+}
+
+export default DisplayIndex;
